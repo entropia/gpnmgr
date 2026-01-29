8567fc20587c9d0742b0e716af7f4df67d530274
promasu <git@promasu.tech>
2026-01-29 23:24:42 +0100

    fix(changelog): potential XSS flaw
    Signed-off-by: promasu <git@promasu.tech>

--- src/gpnmgr/utils/templatetags/changelog.py
+++ src/gpnmgr/utils/templatetags/changelog.py
@@ -1,8 +1,9 @@
 from django import template
-from django.utils.safestring import SafeString, mark_safe
+from django.template.defaultfilters import linebreaksbr
+from django.utils.safestring import SafeString
 
 register = template.Library()
 
 @register.filter
 def changelog(value: str) -> SafeString:
-    return mark_safe(value.replace('; ', '<br/ >'))
+    return linebreaksbr(value.replace('; ', '\n'))
