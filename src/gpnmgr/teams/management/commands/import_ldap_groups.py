8db20b641eb948759939b25a8a136a880f727357
promasu <git@promasu.tech>
2026-03-07 17:25:25 +0100

    feat(import_ldap_groups): Add flag to create groups on sync
    Signed-off-by: promasu <git@promasu.tech>

--- src/gpnmgr/teams/management/commands/import_ldap_groups.py
+++ src/gpnmgr/teams/management/commands/import_ldap_groups.py
@@ -13,8 +13,9 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--dry-run', action='store_true', help='Don\'t write any changes')
+        parser.add_argument('--autocreate-teams', action='store_true', help='Automatically create teams if they don\'t exist')
 
-    def handle(self, *args, dry_run, **options):
+    def handle(self, *args, dry_run, autocreate_teams, **options):
         if dry_run:
             print('DRY RUN')
         conn = settings.LDAP_CONNECTION
@@ -45,7 +46,7 @@ class Command(BaseCommand):
                 'group': group_name
             })
 
-            if not dry_run:
+            if autocreate_teams and not dry_run:
                 team, created = Team.objects.get_or_create(ldap_name=group_name)
                 if created:
                     team.slug = group_name
@@ -55,19 +56,23 @@ class Command(BaseCommand):
             else:
                 team = Team.objects.filter(ldap_name=group_name).first()
                 if team is None:
-                    print(_('Team not existing, would create'))
+                    if autocreate_teams:
+                        print(_('Team not existing, would create'))
+                    else:
+                        print(_('Team not existing, skipping'))
 
             print(_('Syncing'))
 
             ldap_group_members = [User.objects.get(username=member.split(',')[0].split('=')[1]) for member in members if LDAP_USER_PK in member]
             ldap_group_owners = [User.objects.get(username=owner.split(',')[0].split('=')[1]) for owner in owners if LDAP_USER_PK in owner]
-            if not dry_run:
-                team.members.set(ldap_group_members)
-                team.admins.set(ldap_group_owners)
-                team.save()
+            if team is not None:
+                if not dry_run:
+                    team.members.set(ldap_group_members)
+                    team.admins.set(ldap_group_owners)
+                    team.save()
 
-            print(f'Add {len(ldap_group_members)} members to team: {team}')
-            print(f'Added {len(ldap_group_owners)} managers to team: {team}')
+                print(f'Add {len(ldap_group_members)} members to team: {team}')
+                print(f'Added {len(ldap_group_owners)} managers to team: {team}')
 
         conn.unbind()
         print("Import complete.")
